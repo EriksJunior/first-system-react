632bde8ac9f3b8e27f15ac8770b51b0f4eaf8a64
EriksJunior <EriksJunior13@gmail.com>
2022-02-16 23:16:44 -0300

    commit

--- src/service/ClienteService/index.js
+++ src/service/ClienteService/index.js
@@ -1,14 +1,21 @@
 import { http } from "../../config/index";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class ClienteFunctions {
   async saveCliente(dataCliente) {
     try {
       const { data } = await http.post("/cliente", dataCliente);
       console.log(data);
+      this.ovo();
       return data;
     } catch (error) {
       console.log(error);
     }
+  }
+
+  ovo() {
+    toast("Wow so easy!");
   }
 }
 
