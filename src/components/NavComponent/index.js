534810a1b1344fd186e3ca179615a4e4adea715e
EriksJunior <EriksJunior13@gmail.com>
2022-02-20 20:29:32 -0300

    commit

--- src/components/NavComponent/index.js
+++ src/components/NavComponent/index.js
@@ -12,7 +12,6 @@ function NavComponent() {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="/navbarNavDropdown"
             aria-controls="navbarNavDropdown"
             aria-expanded="false"
             aria-label="Toggle navigation"
